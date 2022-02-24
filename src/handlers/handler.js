a586416c92f06e36d92931feb844432883c66f86
Daniel Rivera <Danielf.r97@gmail.com>
2022-02-23 21:01:57 -0500

    initial example

---
+++ src/handlers/handler.js
@@ -0,0 +1,16 @@
+"use strict";
+const a = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v1.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.hello = a;
